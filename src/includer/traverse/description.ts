b2170567d1f66be336d10630803241ed28cb122e
Evgenii Fedoseev <zorox112@yandex-team.ru>
2024-03-24 17:32:54 +0300

    feat: new design, custom descriptions

---
+++ src/includer/traverse/description.ts
@@ -0,0 +1,119 @@
+import {EOL} from '../constants';
+import {OpenJSONSchema} from '../models';
+import {concatNewLine} from '../utils';
+
+type Field = {
+    key: keyof OpenJSONSchema;
+    label: string;
+    computed?: unknown;
+    notWrapValueIntoCode?: boolean;
+};
+
+type Fields = (Field | ((value: OpenJSONSchema) => Field | undefined))[];
+
+/* @todo add i18n */
+const fields: Fields = [
+    {
+        key: 'default',
+        label: 'Default',
+    },
+    {
+        key: 'example',
+        label: 'Example',
+    },
+    {
+        key: 'minLength',
+        label: 'Min length',
+    },
+    {
+        key: 'maxLength',
+        label: 'Max length',
+    },
+    {
+        key: 'maxItems',
+        label: 'Max items',
+    },
+    {
+        key: 'minItems',
+        label: 'Min items',
+    },
+    {
+        key: 'pattern',
+        label: 'Pattern',
+    },
+    {
+        key: 'uniqueItems',
+        label: 'Unique items',
+    },
+    (value) => {
+        return {
+            key: 'minimum',
+            label: `Min value${value.exclusiveMinimum ? ' (exclusive)' : ''}`,
+        };
+    },
+    (value) => {
+        return {
+            key: 'maximum',
+            label: `Max value${value.exclusiveMaximum ? ' (exclusive)' : ''}`,
+        };
+    },
+    (value: OpenJSONSchema) => {
+        const enumValues = value.enum?.map((s) => `\`${s}\``).join(', ');
+
+        if (!enumValues) {
+            return undefined;
+        }
+
+        return {
+            computed: enumValues as Field['key'],
+            notWrapValueIntoCode: true,
+            key: 'enum',
+            label: 'Enum',
+        };
+    },
+];
+
+function prepareComplexDescription(baseDescription: string, value: OpenJSONSchema): string {
+    const description = baseDescription + EOL;
+
+    return fields.reduce((acc, curr) => {
+        const field = typeof curr === 'function' ? curr(value) : curr;
+
+        if (typeof field === 'undefined' || !value[field.key]) {
+            return acc;
+        }
+
+        const {key, label, computed, notWrapValueIntoCode} = field;
+
+        return concatConstraint(acc, computed || value[key], label + ':', notWrapValueIntoCode);
+    }, description);
+}
+
+function concatConstraint(
+    description: string,
+    constraint: unknown,
+    constraintLabel: string,
+    notWrapValueIntoCode = false,
+) {
+    if (typeof constraint !== 'undefined') {
+        return concatNewLine(
+            description,
+            `<span class="openapi-description-annotation">${constraintLabel}</span> ${prepareConstraintValue(
+                constraint,
+                notWrapValueIntoCode,
+            )}`,
+        );
+    }
+
+    return description;
+}
+
+function prepareConstraintValue(value: unknown, notWrapValueIntoCode: boolean) {
+    if (typeof value === 'boolean') {
+        return '';
+    }
+    return notWrapValueIntoCode ? value : `\`${value}\``;
+}
+
+export {fields, prepareComplexDescription};
+export default {fields, prepareComplexDescription};
