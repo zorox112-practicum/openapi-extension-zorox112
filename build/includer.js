66a4e37bd2cc93ba57e2b1e18fb5bed5addb5810
3y3 <3y3@ya.ru>
2024-04-03 19:08:51 +0300

    chore: Downgrade glob version. Fix metapackage

--- build/includer.js
+++ build/includer.js
@@ -1,4 +1,4 @@
-const {globSync} = require('glob');
+const glob = require('glob');
 const path = require('node:path');
 const esbuild = require('esbuild');
 
@@ -6,7 +6,7 @@ const IN_PATH = path.resolve('src', 'includer');
 const OUT_PATH = 'includer';
 const FORMATS = ['cjs', 'esm'];
 
-const tsFiles = globSync(`${IN_PATH}/**/*.ts`);
+const tsFiles = glob.sync(`${IN_PATH}/**/*.ts`);
 
 const build = (files, format) => Promise.all(files.map((name) => {
     const newFilePath = path.resolve(OUT_PATH, format, name.substring(IN_PATH.length + 1));
